9fc483b8c4bc9a39d75184ff7d33186f3c339eaf
Tony Morris <tmorris@tmorris.net>
2013-11-15 10:26:06 +1000

    .

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -16,16 +16,19 @@ class Functor f => Extend f where
 
 infixr 1 <<=
 
+-- | Implement the @Extend@ instance for @Id@.
 instance Extend Id where
   f <<= i =
     Id (f i)
 
+-- | Implement the @Extend@ instance for @List@.
 instance Extend List where
   _ <<= Nil =
     Nil
   f <<= x@(_ :. t) =
     f x :. (f <<= t)
 
+-- | Implement the @Extend@ instance for @Optional@.
 instance Extend Optional where
   f <<= o =
     f . Full <$> o
