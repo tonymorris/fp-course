bdb30bde5dc850a605626142f436548d66115b4b
Tony Morris <tmorris@tmorris.net>
2015-10-14 13:31:07 +1000

    Monad answers

--- src/Course/Monad.hs
+++ src/Course/Monad.hs
@@ -67,8 +67,8 @@ infixr 1 =<<
   f (a -> b)
   -> f a
   -> f b
-(<*>) =
-  error "todo: Course.Monad#(<*>)"
+f <*> a =
+  (\f' -> return . f' =<< a) =<< f
 
 infixl 4 <*>
 
@@ -81,8 +81,8 @@ instance Monad Id where
     (a -> Id b)
     -> Id a
     -> Id b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance Id"
+  f =<< Id a =
+    f a
 
 -- | Binds a function on a List.
 --
@@ -94,7 +94,7 @@ instance Monad List where
     -> List a
     -> List b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance List"
+    flatMap
 
 -- | Binds a function on an Optional.
 --
@@ -106,7 +106,7 @@ instance Monad Optional where
     -> Optional a
     -> Optional b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance Optional"
+    bindOptional
 
 -- | Binds a function on the reader ((->) t).
 --
@@ -117,8 +117,8 @@ instance Monad ((->) t) where
     (a -> ((->) t b))
     -> ((->) t a)
     -> ((->) t b)
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance ((->) t)"
+  f =<< g =
+    \t -> f (g t) t
 
 -- | Flattens a combined structure to a single structure.
 --
@@ -138,7 +138,7 @@ join ::
   f (f a)
   -> f a
 join =
-  error "todo: Course.Monad#join"
+  (=<<) id
 
 -- | Implement a flipped version of @(=<<)@, however, use only
 -- @join@ and @(<$>)@.
@@ -167,8 +167,8 @@ infixl 1 >>=
   -> (a -> f b)
   -> a
   -> f c
-(<=<) =
-  error "todo: Course.Monad#(<=<)"
+f <=< g =
+  (=<<) f . g
 
 infixr 1 <=<
 
